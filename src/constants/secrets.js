2c31963dd93a651d26213f599f7c7221358b24f8
Muskan <muskan8191822@gmail.com>
2026-01-05 15:27:22 +0530

    Removed a file from version control

--- src/constants/secrets.js
+++
@@ -1,6 +0,0 @@
-const MONGO_URI= "mongodb+srv://namasteDev:0807@namastenode.dsahmfb.mongodb.net/devTinder";
-const JWT_SECRET = "DEV@Tinder$790";
-module.exports= {
-    MONGO_URI,
-    JWT_SECRET,
-};
